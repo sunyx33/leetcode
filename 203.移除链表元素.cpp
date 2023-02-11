567a88c11b5a61d9b92e86fe525fb5207cb6755d
sunyx33 <sunyx33@163.com>
2023-02-11 23:16:34 +0800

    23.2.11

---
+++ 203.移除链表元素.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=cpp
+ *
+ * [203] 移除链表元素
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummyHead = new ListNode(0, head);
+        ListNode* cur = dummyHead;
+        while (cur->next) {
+            if (cur->next->val == val){
+                ListNode* tmp = cur->next;
+                cur->next = cur->next->next;
+                delete tmp;
+            } else {
+                cur = cur->next;
+            }
+        }
+        head = dummyHead->next;
+        delete dummyHead;
+        return head;
+    }
+};
+// @lc code=end
+
