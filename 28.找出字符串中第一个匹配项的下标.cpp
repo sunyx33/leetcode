f66711a9de9908fb8615c7514562c05eeae62086
sunyx33 <sunyx33@163.com>
2023-02-15 15:28:10 +0800

    23.2.15

---
+++ 28.找出字符串中第一个匹配项的下标.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=28 lang=cpp
+ *
+ * [28] 找出字符串中第一个匹配项的下标
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void getNext(int next[], const string& s) {
+        int j = 0;
+        next[0] = 0;
+        for(int i = 1; i < s.size(); i++) {
+            while (j > 0 && s[i] != s[j]) {
+                j = next[j - 1]; // 若当前两个不同，则找次长相同前后缀
+            }
+            if (s[i] == s[j]) {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
+    int strStr(string haystack, string needle) {
+        if (needle.size() == 0) {
+            return 0;
+        }
+        int next[needle.size()];
+        getNext(next, needle);
+        int j = 0;
+        for (int i = 0; i < haystack.size(); i++) {
+            while(j > 0 && haystack[i] != needle[j]) {
+                j = next[j - 1];
+            }
+            if (haystack[i] == needle[j]) {
+                j++;
+            }
+            if (j == needle.size() ) {
+                return (i - needle.size() + 1);
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
+int main(){
+    string s = "aabaabaaf";
+    string a = "aabf";
+    Solution sol;
+    sol.strStr(a, s);
+
+    return 0;
+
+}
---
+++ 459.重复的子字符串.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=459 lang=cpp
+ *
+ * [459] 重复的子字符串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void getNext(int* next, string s){
+        int j = 0;
+        next[0] = 0;
+        for (int i = 1; i < s.size(); i++) {
+            while(j != 0 && s[i] != s[j]) {
+                j = next[j - 1];
+            } 
+
+            if (s[i] == s[j]) {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
+
+    bool repeatedSubstringPattern(string s) {
+        if (s.size() == 0) {
+            return false;
+        }    
+        int next[s.size()];
+        getNext(next, s);
+        int len = s.size();
+        if(next[len - 1] != 0 && len % (len - next[len - 1]) == 0) {
+            return true;
+        }
+
+        return false;
+    }
+};
+// @lc code=end
+
