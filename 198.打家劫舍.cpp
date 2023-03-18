f6e486f90de438dbf6d044f07ecaff00fd90872b
sunyx33 <sunyx33@163.com>
2023-03-18 12:48:43 +0800

    23.3.17

---
+++ 198.打家劫舍.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=cpp
+ *
+ * [198] 打家劫舍
+ */
+#include <vector>
+
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size() == 1) return nums[0];
+        vector<int> dp(nums.size(), 0); 
+        
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); i++){
+           dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+
+        return dp[dp.size() - 1];
+    }
+};
+// @lc code=end
+
